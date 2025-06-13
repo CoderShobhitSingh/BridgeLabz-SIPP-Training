8f912652a69632fe7172d9d8d3c1dff6951b90f1
CoderShobhitSingh <snghshobhit@gmail.com>
2025-06-13 10:38:46 +0530

    PenDistribution

---
+++ Day_2_Level_1_practice/PenDistribution.java
@@ -0,0 +1,13 @@
+package Day_2_Level_1_practice;
+
+public class PenDistribution {
+    public static void main(String[] args) {
+        int totalPens = 14;
+        int students = 3;
+
+        int pensPerStd = totalPens / students;
+        int remaining = totalPens % students;
+
+        System.out.println("The pen per student is "+pensPerStd+" and the remaining pens are not distributed is "+remaining);
+    }
+}
