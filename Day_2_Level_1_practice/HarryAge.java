0055a0f686e1f0bde30ae6f707120954ad274eb8
CoderShobhitSingh <snghshobhit@gmail.com>
2025-06-13 09:49:47 +0530

    HarryAge

---
+++ Day_2_Level_1_practice/HarryAge.java
@@ -0,0 +1,11 @@
+package Day_2_Level_1_practice;
+
+public class HarryAge {
+    public static void main(String[] args) {
+        int birthYear = 2000;
+    int currentYear = 2025;
+
+    int age = birthYear - currentYear;
+    System.out.println("Harry's age in 2024 is "+age);
+    }
+}
