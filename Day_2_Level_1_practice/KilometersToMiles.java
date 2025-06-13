6aa7470bb4deb2d2208d40292abf699ca700e2d2
CoderShobhitSingh <snghshobhit@gmail.com>
2025-06-13 10:01:03 +0530

    KmToMiles

---
+++ Day_2_Level_1_practice/KilometersToMiles.java
@@ -0,0 +1,13 @@
+package Day_2_Level_1_practice;
+
+import java.util.Scanner;
+
+public class KilometersToMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter distance in km: ");
+        double km = sc.nextDouble();
+        double miles = km / 1.6;
+        System.out.println("The total miles is " + miles + " mile for the given " + km + " km");
+    }
+}
