e7a34ebe4708e14c250dc30f6c33d44cb1f59eed
CoderShobhitSingh <snghshobhit@gmail.com>
2025-06-13 09:48:19 +0530

    FindSide

---
+++ Day_2_Level_1_practice/FindSide.java
@@ -0,0 +1,13 @@
+package Day_2_Level_1_practice;
+
+import java.util.Scanner;
+
+public class FindSide {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter perimeter: ");
+        double perimeter = input.nextDouble();
+        double side = perimeter / 4;
+        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+    }
+}
