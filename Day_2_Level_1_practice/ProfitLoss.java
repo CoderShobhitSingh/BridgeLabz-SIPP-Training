6fc78c1884047e2b628c548eb3744155d9ae5b93
CoderShobhitSingh <snghshobhit@gmail.com>
2025-06-13 10:47:13 +0530

    ProfitLoss

---
+++ Day_2_Level_1_practice/ProfitLoss.java
@@ -0,0 +1,12 @@
+package Day_2_Level_1_practice;
+
+public class ProfitLoss {
+    public static void main(String[] args) {
+        int costPrice = 129;
+        int sellingPrice = 191;
+
+        int Profit = sellingPrice - costPrice;
+        int profitPercentage = Profit / costPrice * 100;
+
+    }
+}
