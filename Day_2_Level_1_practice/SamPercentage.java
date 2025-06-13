d5f5865cc74281e21495916d8d21dbb938f49f43
CoderShobhitSingh <snghshobhit@gmail.com>
2025-06-13 10:59:10 +0530

    SamPercentage

---
+++ Day_2_Level_1_practice/SamPercentage.java
@@ -0,0 +1,9 @@
+package Day_2_Level_1_practice;
+
+public class SamPercentage {
+    public static void main(String[] args) {
+        int maths = 94, physics = 95, chemistry = 96;
+        double average = (maths + physics + chemistry) / 3.0;
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
