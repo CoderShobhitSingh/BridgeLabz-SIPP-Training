c2768c3f8e7dc15a7100c0614e35a44243fb58f4
CoderShobhitSingh <snghshobhit@gmail.com>
2025-07-14 12:11:20 +0530

    Delete Day_5_Encapsulation_Polymorphism_Abstract directory

--- Day_5_Encapsulation_Polymorphism_Abstract/E_CommercePlatform/Taxable.java
+++
@@ -1,7 +0,0 @@
-package Day_5_Encapsulation_Polymorphism_Abstract.E_CommercePlatform;
-
-public interface Taxable {
-    double calculateTax();
-    String getTaxDetails();
-}
-
--- Day_5_Encapsulation_Polymorphism_Abstract/EmployeeManagementSystem/Department.java
+++
@@ -1,7 +0,0 @@
-package Day_5_Encapsulation_Polymorphism_Abstract.EmployeeManagementSystem;
-
-public interface Department {
-    void assignDepartment(String dept);
-    String getDepartmentDetails();
-}
-
