d565f97fe384c35f545a2983ce87039a534f708e
CoderShobhitSingh <snghshobhit@gmail.com>
2025-07-28 06:22:05 +0530

    [Shobhit] .Add peak element

---
+++ Day_4_SearchingAlgorithms/PeakElement.java
@@ -0,0 +1,20 @@
+package Day_4_SearchingAlgorithms;
+
+public class PeakElement {
+    public static int findPeak(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < arr[mid + 1]) left = mid + 1;
+            else right = mid;
+        }
+        return arr[left];  
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 20, 4, 1};
+        int peak = findPeak(arr);
+        System.out.println("Peak element is: " + peak);
+    }
+}
+
